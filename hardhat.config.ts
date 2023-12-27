0f8073f2e741453797290ffc0051a8d1734a055e
esdras-santos <esantoz@protonmail.com>
2023-12-27 11:05:38 -0300

    added different compiler versions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,29 +8,75 @@ import "solidity-coverage"
 
 const config: HardhatUserConfig = {
     solidity: {
-        version: "0.7.6",
-        settings: {
-            optimizer: { enabled: true, runs: 200 },
-            evmVersion: "berlin",
-            // for smock to mock contracts
-            outputSelection: {
-                "*": {
-                    "*": ["storageLayout"],
+        compilers: [
+            {
+                version: "0.7.6",
+                settings: {
+                    optimizer: { enabled: true, runs: 200 },
+                    evmVersion: "berlin",
+                    // for smock to mock contracts
+                    outputSelection: {
+                        "*": {
+                            "*": ["storageLayout"],
+                        },
+                    },
                 },
             },
-        },
+            {
+                version: "0.8.17",
+                settings: {
+                    // for smock to mock contracts
+                    outputSelection: {
+                        "*": {
+                            "*": ["storageLayout"],
+                        },
+                    },
+                },
+            },
+            {
+                version: "0.8.20",
+                settings: {
+                    // for smock to mock contracts
+                    outputSelection: {
+                        "*": {
+                            "*": ["storageLayout"],
+                        },
+                    },
+                },
+            },
+            {
+                version: "0.6.2",
+                settings: {
+                    // for smock to mock contracts
+                    outputSelection: {
+                        "*": {
+                            "*": ["storageLayout"],
+                        },
+                    },
+                },
+            },
+            {
+                version: "0.8.12",
+                settings: {
+                    // for smock to mock contracts
+                    outputSelection: {
+                        "*": {
+                            "*": ["storageLayout"],
+                        },
+                    },
+                },
+            }
+        ]
+        
     },
     mocha: {
         timeout: 100000,
     },
+    
     networks: {
         hardhat: {
             allowUnlimitedContractSize: true,
         },
-    },
-    dependencyCompiler: {
-        // We have to compile from source since UniswapV3 doesn't provide artifacts in their npm package
-        paths: ["@uniswap/v3-core/contracts/UniswapV3Factory.sol", "@uniswap/v3-core/contracts/UniswapV3Pool.sol"],
     },
     gasReporter: {
         enabled: true,
